ee4d4777326cdd5ffb6551c5b1a268ad08c347ee
alsaeed3 <ali.eissa.570@gmail.com>
2024-04-26 00:52:25 +0400

    check push

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 09:06:01 by alsaeed           #+#    #+#             */
-/*   Updated: 2024/04/17 10:38:39 by alsaeed          ###   ########.fr       */
+/*   Updated: 2024/04/18 07:06:16 by alsaeed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ Harl::~Harl( void ) {
 void	Harl::debug( void ) {
 
 	std::cout << "[ DEBUG ]" << std::endl;
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
+	std::cout << "I really do!" << std::endl;
 	std::cout << std::endl;
 
 	return;	
@@ -34,7 +35,8 @@ void	Harl::debug( void ) {
 void	Harl::info( void ) {
 
 	std::cout << "[ INFO ]" << std::endl;
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 	std::cout << std::endl;
 
 	return;	
@@ -43,7 +45,8 @@ void	Harl::info( void ) {
 void	Harl::warning( void ) {
 
 	std::cout << "[ WARNING ]" << std::endl;
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl;
 	std::cout << std::endl;
 
 	return;	
